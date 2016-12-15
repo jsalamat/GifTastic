1b008eea9ee1d71dccc23fa6a16faf63cb5ec30a
Jon Salamat <Jon Salamat>
2016-12-14 21:37:41 -0500

    made on click gif and now still to animate

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,72 +1,97 @@
-var topics =["Dragonball Super", "Cowboy Bebop", "One Piece", "Samurai Champloo", "One-Punch Man", "Attack on Titans", "Howl's Moving Castle", "Ninja Scroll", "Ghost In the Shell", "Akira"]
+$(document).ready(function(){
 
-    function displayanimeGif() {
+  // topics in the array to be a button
 
-    var animename = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animename + "&api_key=dc6zaTOxFJmzC&limit=10";
+        var topics =["Dragonball Super", "Cowboy Bebop", "One Piece", "Samurai Champloo", "One-Punch Man", "Attack on Titans", "Howl's Moving Castle", "Ninja Scroll", "Ghost In the Shell", "Sailor Moon"]
 
-                                                        // q= search query term or phrase   limit=10 limits the results returned to 10
+        function displayanimeGif() {
 
-    // Creates AJAX call for the specific movie button being clicked
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).done(function(response) {
-      $('#anime-view').empty();
-      console.log(response);
-      
-      var results = response.data;
+        var animename = $(this).attr("data-name");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animename + "&api_key=dc6zaTOxFJmzC&limit=10";
 
-          for (var i = 0; i < results.length; i++) {
+                                                            // q= search query term or phrase   limit=10 limits the results returned to 10
 
-          // Creates a div to hold the movie
-          var animenameDiv = $('<div class="animename">')
-          // Creating a paragraph tag with the result item's rating
-          var pRate = $('<p>').text("Rating: " + results[i].rating);
-          // Displays the rating
-          animenameDiv.append(pRate);
-          // Creating and storing an image tag
-          var animeImage = $("<img>");
-          // Setting the src attribute of the image to a property pulled off the result item
-           animeImage.attr("src", results[i].images.fixed_height.url)
-           //adding attribute data-still and data animate
-           // adding attribute data-state=still and class= "gif"
-           animeImage.attr("data-still", "").attr("data-animate", "");
-           // adding attribute data-state=still and class= "gif"
-           animeImage.attr("data-state", "still").attr("class", "gif");
-          // Appending the paragraph and image tag to the animenameDiv
-          animenameDiv.append(animeImage);
-          animenameDiv.append(pRate);
-          // Prependng the animenameDiv to the HTML page in the "#anime-view" div
-          $("#anime-view").prepend(animenameDiv);
+        // Creates AJAX call for the specific movie button being clicked
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+          $('#anime-view').empty();
+          console.log(response);
+          
+          var results = response.data;
+
+              for (var i = 0; i < results.length; i++) {
+
+              // Creates a div to hold the movie
+              var animenameDiv = $('<div class="animename">')
+              // Creating a paragraph tag with the result item's rating
+              var pRate = $('<p>').text("Rating: " + results[i].rating);
+              // Displays the rating
+              animenameDiv.append(pRate);
+              // Creating and storing an image tag
+              var animeImage = $("<img>");
+              // Setting the src attribute of the image to a property pulled off the result item
+               animeImage.attr("src", results[i].images.fixed_height_still.url)
+               //adding attribute data-still and data animate
+
+      // This atributes are needed in image for the gif to have a function
+               // adding attribute data-state=still and class= "gif"
+               animeImage.attr("data-still", results[i].images.fixed_height_still.url).attr("data-animate", results[i].images.fixed_height.url);
+               // adding attribute data-state=still and class= "gif"
+               animeImage.attr("data-state", "still").attr("class", "gif");
+
+              // Appending the paragraph and image tag to the animenameDiv
+              animenameDiv.append(animeImage);
+              animenameDiv.append(pRate);
+              // Prependng the animenameDiv to the HTML page in the "#anime-view" div
+              $("#anime-view").prepend(animenameDiv);
+            }
+
+            $(".gif").on("click", function() {
+              // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+              var state = $(this).attr("data-state");
+              
+              if (state === "still") {
+                $(this).attr("src", $(this).data("animate"));
+                $(this).attr("data-state", "animate");
+              } else {
+                // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+                // Then, set the image's data-state to animate
+                $(this).attr("src", $(this).data("still"));
+                $(this).attr("data-state", "still");
+              }
+            });
+
+        });
+
+      }
+
+        function renderButtons() {
+            //delete the contents inside anime-view
+            $('#buttons-view').empty();
+            // Loop to array of anime variable and generate eah anime in the array
+            for (i = 0; i <topics.length; i++) {
+                var a = $('<button>')
+                a.addClass('anime');
+                a.attr('data-name', topics[i]);
+                a.text(topics[i]);
+                $('#buttons-view').append(a);
+            }
         }
-    });
 
-  }
+        $('#add-anime').on("click", function(event){
+            event.preventDefault();
 
-    function renderButtons() {
-        //delete the contents inside anime-view
-        $('#buttons-view').empty();
-        // Loop to array of anime variable and generate eah anime in the array
-        for (i = 0; i <topics.length; i++) {
-            var a = $('<button>')
-            a.addClass('anime');
-            a.attr('data-name', topics[i]);
-            a.text(topics[i]);
-            $('#buttons-view').append(a);
-        }
-    }
+            var anime = $('#anime-input').val().trim();
 
-    $('#add-anime').on("click", function(event){
-        event.preventDefault();
+            topics.push(anime);
 
-        var anime = $('#anime-input').val().trim();
+            renderButtons();
+        })
 
-        topics.push(anime);
+        $(document).on("click", ".anime", displayanimeGif)
 
-        renderButtons();
-    })
+    renderButtons();
 
-    $(document).on("click", ".anime", displayanimeGif)
-
-renderButtons();
+});
